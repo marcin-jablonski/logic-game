c7ceedb4406b953322e5f82b6b12809d5fc1a440
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-04-13 03:35:14 +0200

    add Third Person Camera and change PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,58 +5,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float _speed = 6f;
+    public float _turnSmoothTime = 0.2f;
+    private float _turnSmoothVelocity;
+    private float _speedSmoothVelocity;
+    private float _currentSpeed;
+    private float _speedSmoothTime = 0.1f;
     private Vector3 _movement;
     private Animator _animator;
-    private Rigidbody _playerRb;
-    private float camRayLength = 100f;
-
+    //private Rigidbody _playerRb;
+    private Transform _cameraTransform;
+        
     void Awake()
     {
         _animator = GetComponent<Animator>();
-        _playerRb = GetComponent<Rigidbody>();
+        //_playerRb = GetComponent<Rigidbody>();
+        _cameraTransform = Camera.main.transform;
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
-        Move(h, v);
-        Turning();
-        Animating(h, v);
+        Move();
     }
-
-    void Move(float h, float v)
-    {
-        _movement.Set(h, 0f, v);
-        _movement = _movement.normalized * _speed * Time.deltaTime;
-
-        _playerRb.MovePosition(transform.position + _movement);
-    }
-
-    void Turning()
-    {
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 playerToMouse = camRay.direction * 10 - transform.position;
-        playerToMouse.y = 0f;
-
-        Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-        _playerRb.MoveRotation(newRotation);
-    }
-
+    
     void Animating(float h, float v)
     {
         bool walking = h != 0f || v != 0f;
         _animator.SetBool("isWalking", walking);
     }
+    
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Move()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + _cameraTransform.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref _turnSmoothVelocity, _turnSmoothTime);
+        }
+
+        float targetSpeed = _speed * inputDir.magnitude;
+        _currentSpeed = Mathf.SmoothDamp(_currentSpeed, targetSpeed, ref _speedSmoothVelocity, _speedSmoothTime);
+
+        transform.Translate(transform.forward * _currentSpeed * Time.deltaTime, Space.World);
+
+        Animating(input.x, input.y);
+    }
 }
