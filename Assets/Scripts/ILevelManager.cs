3f639f86d68589518803064fc3879a4ec6d67bf7
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-04-23 01:01:05 +0200

    simple Lvl1 manager

--- Assets/Scripts/ILevelManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-
-public interface ILevelManager {
-
-    void LogCollision(object oSender, EventArgs oEventArgs);
-}
--- Assets/Scripts/Level1/Level1Manager.cs
+++ Assets/Scripts/Level1/Level1Manager.cs
@@ -2,21 +2,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Level1Manager : LevelManager {
+
+    private List<String> _correctOrder = new List<String>{
+        "Block01", "Block02", "Block03", "Block04", "Block05", "Block06",
+        "Block07", "Block08", "Block09","Block10", "Block11", "Block12",
+        "Block13", "Block14", "Block15", "Block16", "Block17", "Block18"
+    };
+
+    private List<String> _playerOrder = new List<String>();
 
     public override void LogCollision(object oSender, EventArgs oEventArgs)
     {
         CollisionArgs oCollisionArgs = oEventArgs as CollisionArgs;
-        Debug.Log(oCollisionArgs.ColliderChild.tag);
+        addBlock(oCollisionArgs.ColliderChild.tag);
+        //Debug.Log(oCollisionArgs.ColliderChild.tag);
+        //oCollisionArgs.ColliderChild.gameObject.SetActive(false);
     }
 
-    //public void LogCollision(object oSender, EventArgs oEventArgs)
-    //{
-    //    CollisionArgs oCollisionArgs = oEventArgs as CollisionArgs;
+    private void addBlock(String blockTag)
+    {
+       
 
-    //    Debug.Log(oCollisionArgs.ColliderParent.tag);
-    //}
+        if (blockTag == _correctOrder.ElementAt(_playerOrder.Count))
+        {
+            Debug.Log("adding: " + blockTag);
+            _playerOrder.Add(blockTag);
+        }
+        else
+        {
+            _playerOrder.Clear();
+            Debug.Log("zle: "+ blockTag);
+        }
+
+  
+        
+    }
+
 
     // Use this for initialization
     //void Start () {
