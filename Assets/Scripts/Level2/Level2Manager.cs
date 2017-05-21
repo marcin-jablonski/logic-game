34f1ec18c34feb381f56069d951b4474b2befebe
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-05-21 18:49:49 +0200

    add empty Level2

---
+++ Assets/Scripts/Level2/Level2Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Manager : LevelManager {
+    public override void LogCollision(object oSender, EventArgs oEventArgs)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    // void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
