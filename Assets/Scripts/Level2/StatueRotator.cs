fa9777444544c99bcd54551fec0e8791fdc9d563
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-05-21 23:07:01 +0200

    fix rotation

--- Assets/Scripts/Level2/StatueRotator.cs
+++ Assets/Scripts/Level2/StatueRotator.cs
@@ -6,21 +6,32 @@ public class StatueRotator : MonoBehaviour {
 
     private float _currentRotation = 0.0f;
 
+    //private float _xRotation = 0.0f;
 
     public void changeRotation()
     {
-        _currentRotation += 90.0f;
+        if (System.Math.Round((decimal)transform.rotation.eulerAngles.y, 0) % 90 == 0)
+        {
+            _currentRotation = (_currentRotation + 90.0f) % 360;
+            if (_currentRotation < 0) _currentRotation += 360;
+        }
+
     }
 
-	void Start () {
-		
+	void Awake () {
+        //Transform go = GetComponent<Transform>();
+        //_xRotation = go.rotation.x;
+        //Debug.Log("X rotation: " + transform.rotation.x);
 	}
+
 
     private void rotate()
     {
         float speed = 0.5f;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, _currentRotation, 0), Time.deltaTime * speed);
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 180, 0), Time.deltaTime * 10f);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(_xRotation, _currentRotation, transform.rotation.z), Time.deltaTime * speed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, _currentRotation, transform.rotation.eulerAngles.z), Time.deltaTime * 20f);
+      
+        
     }
 
     // Update is called once per frame
