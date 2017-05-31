d0469ab2bcaeb5738e43aa3a2e768f9eb04d2b36
Kamil Kazmierczak <kamil.kazmierczak@hotmail.com>
2017-06-01 00:24:41 +0200

    lvl1 blocks detection change (still wrong)

--- Assets/Scripts/Level1/Level1Manager.cs
+++ Assets/Scripts/Level1/Level1Manager.cs
@@ -9,9 +9,9 @@ using UnityEngine.SceneManagement;
 public class Level1Manager : LevelManager {
 
     private List<String> _correctOrder = new List<String>{
-        "Block01", "Block02", "Block03", "Block04", "Block05", "Block06",
-        "Block07", "Block08", "Block09", "Block10", "Block11", "Block12",
-        "Block13", "Block14", "Block15", "Block16", "Block17", "Block18"
+        "Block01y", "Block02r", "Block03r", "Block04r", "Block05r", "Block06b",
+        "Block07b", "Block08b", "Block09g", "Block10g", "Block11g", "Block12g",
+        "Block13b", "Block14b", "Block15r", "Block16r", "Block17r", "Block18x"
     };
 
     private List<Collider> _playerOrder = new List<Collider>();
@@ -24,20 +24,32 @@ public class Level1Manager : LevelManager {
         addBlock(oCollisionArgs.ColliderChild);
     }
 
+
+    private int curr = 0;
+    private String currBlock = "";
+    private String nextBlock = "";
+
     private void addBlock(Collider block)
     {
+        //Debug.Log("jo: "+block.tag[block.tag.Length-1]);
 
-        if (block.tag == _correctOrder.ElementAt(_playerOrder.Count))
+        if (block.tag == _correctOrder.ElementAt(curr) && _playerOrder.Count == curr)
         {
-            Debug.Log("adding: " + block.tag);
-            _playerOrder.Add(block);
+            currBlock = _correctOrder.ElementAt(curr);
+            nextBlock = _correctOrder.ElementAt(++curr);
+            //curr++;
+            Debug.Log("current: " + currBlock);
+            Debug.Log("next: " + nextBlock);
+
             block.gameObject.SetActive(false);
-            if (_correctOrder.Count == _playerOrder.Count)
-            {
-                winText.text = "Zwycięstwo";
-                SceneManager.LoadScene("Level2");
-            }
-                
+            _playerOrder.Add(block);
+
+        }
+        else if (curr>0 && block.tag[block.tag.Length - 1] == nextBlock[nextBlock.Length - 1])
+        {
+            Debug.Log("hiii");
+            block.gameObject.SetActive(false);
+            _playerOrder.Add(block);
         }
         else
         {
@@ -46,9 +58,62 @@ public class Level1Manager : LevelManager {
                 val.gameObject.SetActive(true);
             }
             _playerOrder.Clear();
-            Debug.Log("zle: " + block.tag);
+            curr = 0;
+            currBlock = "";
+            nextBlock = "";
         }
-  
+
+
+        //if (block.tag == _correctOrder.ElementAt(_playerOrder.Count)
+        //    || block.tag[block.tag.Length - 1] == _correctOrder.ElementAt(_playerOrder.Count)[_correctOrder.ElementAt(_playerOrder.Count).Length-1])
+
+        //{
+        //    Debug.Log("adding: " + block.tag);
+        //    _playerOrder.Add(block);
+        //    block.gameObject.SetActive(false);
+
+
+        //    if (_correctOrder.Count == _playerOrder.Count)
+        //    {
+        //        winText.text = "Zwycięstwo";
+        //        SceneManager.LoadScene("Level2");
+        //    }
+
+        //}
+
+        //else
+        //{
+        //    foreach (Collider val in _playerOrder)
+        //    {
+        //        val.gameObject.SetActive(true);
+        //    }
+        //    _playerOrder.Clear();
+        //    Debug.Log("zle: " + block.tag);
+        //}
+
+
+        //if (block.tag == _correctOrder.ElementAt(_playerOrder.Count))
+        //{
+        //    Debug.Log("adding: " + block.tag);
+        //    _playerOrder.Add(block);
+        //    block.gameObject.SetActive(false);
+        //    if (_correctOrder.Count == _playerOrder.Count)
+        //    {
+        //        winText.text = "Zwycięstwo";
+        //        SceneManager.LoadScene("Level2");
+        //    }
+
+        //}
+        //else
+        //{
+        //    foreach (Collider val in _playerOrder)
+        //    {
+        //        val.gameObject.SetActive(true);
+        //    }
+        //    _playerOrder.Clear();
+        //    Debug.Log("zle: " + block.tag);
+        //}
+
     }
 
     // Use this for initialization
